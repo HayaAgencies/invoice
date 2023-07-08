e58f176bcd0c65e98237b4e7cd62759fef1ad6ff
rishan987 <riyashasanath@gmail.com>
2023-07-08 20:40:55 +0530

    hi

--- script.js
+++ script.js
@@ -9,27 +9,28 @@ function AddRow(){
 }
 function Deleter(v){
     $(v).parent().parent().remove();
+    GetTotal()
 }
-function GetTot(v)
+function gtem(v)
 {
     var index = $(v).parent().parent().index();
     
-    var qty = document.getElementsByName("Qty")[index].value;
-    var rate = document.getElementsByName("Rate")[index].value;
+    var qty = document.getElementsByName("qty")[index].value;
+    var rate = document.getElementsByName("rate")[index].value;
 
     var amt = qty * rate
-    document.getElementsByName("Amount")[index].value = amt
+    document.getElementsByName("amount")[index].value = amt
     GetTotal();
 }
 function GetTotal()
 {
     var  sum = 0;
-    var amts= document.getElementsByName("Amount")
+    var amts= document.getElementsByName("amount")
 
     for(let index = 0; index <amts.length; index++)
     {
         var amt = amts[index].value
         sum = + (sum)+ +(amt);
     }
-document.getElementById("Ftotal").value = sum
+document.getElementById("Ftotal").value = sum;
 }
