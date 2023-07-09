38b83df0d01c2b963e5341d9f3fba538d55ebc82
rishan987 <riyashasanath@gmail.com>
2023-07-09 14:05:33 +0530

    updates

--- script.js
+++ script.js
@@ -60,3 +60,19 @@ function GetTotal()
 
 }
  
+$(document).ready(function () {
+    FillDataList();
+});
+
+function FillDataList()
+{
+        $.getJSON("https://script.google.com/macros/s/AKfycbznmBRZdKIKDjKYsTNN63Clu9KsIkVqM0TEXmOtog7WIpRkL2fIuK9cYhW3d7CCL1qYgw/exec?page=dropdown", 
+        function (data) {                              //01
+          var Options="";                              
+          $.each(data, function(key, value)            //02
+          {
+            Options = Options + '<option>' + value + '</option>';   //03
+          });
+          $(".item_nm").append(Options);               //04
+        });
+}
