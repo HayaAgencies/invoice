988e6053d4f927177a3332b31212fb52f7a9cdbc
rishan987 <riyashasanath@gmail.com>
2023-07-09 13:15:55 +0530

    ALL UPDATE
    I ADDED SOME REPORTS

--- script.js
+++ script.js
@@ -1,36 +1,67 @@
+
 function GetPrint()
 {
+    /*For Print*/
     window.print();
 }
-function AddRow(){
-    var v =  $("#Trow").clone().appendTo("#Tbody");
-    $(v).find('input').val('');
-    $(v).removeClass("d-none");
-}
-function Deleter(v){
-    $(v).parent().parent().remove();
-    GetTotal()
-}
-function gtem(v)
+
+function BtnAdd()
 {
+    /*Add Button*/
+    var v = $("#TRow").clone().appendTo("#TBody") ;
+    $(v).find("input").val('');
+    $(v).removeClass("d-none");
+    $(v).find("th").first().html($('#TBody tr').length - 1);
+}
+
+function BtnDel(v)
+{
+    /*Delete Button*/
+       $(v).parent().parent().remove(); 
+       GetTotal();
+
+        $("#TBody").find("tr").each(
+        function(index)
+        {
+           $(this).find("th").first().html(index);
+        }
+
+       );
+}
+
+function Calc(v)
+{
+    /*Detail Calculation Each Row*/
     var index = $(v).parent().parent().index();
     
     var qty = document.getElementsByName("qty")[index].value;
     var rate = document.getElementsByName("rate")[index].value;
 
-    var amt = qty * rate
-    document.getElementsByName("amount")[index].value = amt
+    var amt = qty * rate;
+    document.getElementsByName("amt")[index].value = amt;
+
     GetTotal();
 }
+
 function GetTotal()
 {
-    var  sum = 0;
-    var amts= document.getElementsByName("amount")
+    /*Footer Calculation*/   
 
-    for(let index = 0; index <amts.length; index++)
+    var sum=0;
+    var amts =  document.getElementsByName("amt");
+
+    for (let index = 0; index < amts.length; index++)
     {
-        var amt = amts[index].value
-        sum = + (sum)+ +(amt);
+        var amt = amts[index].value;
+        sum = +(sum) +  +(amt) ; 
     }
-document.getElementById("Ftotal").value = sum;
+
+    document.getElementById("FTotal").value = sum;
+
+    var gst =  document.getElementById("FGST").value;
+    var net = +(sum) + +(gst);
+    document.getElementById("FNet").value = net;
+
+
 }
+ 
